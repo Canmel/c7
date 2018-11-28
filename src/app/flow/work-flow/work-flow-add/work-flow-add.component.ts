741ea783bdf19a72098b28531d724575a20d05c0
lidejian <lidj@c-sunrise.com>
2018-11-28 18:24:26 +0800

    camunda 等bpmn插件

--- src/app/flow/work-flow/work-flow-add/work-flow-add.component.ts
+++ src/app/flow/work-flow/work-flow-add/work-flow-add.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import propertiesPanelModule from 'bpmn-js-properties-panel';
-import { Http } from '@angular/http';
-import { DomSanitizer } from '@angular/platform-browser';
+import {Http} from '@angular/http';
+import {DomSanitizer} from '@angular/platform-browser';
 import propertiesProvider from 'bpmn-js-properties-panel/lib/provider/camunda';
 import {CamundaModdleDescriptor} from '../../../../public/config/camunda_moddle_descriptor';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-work-flow-add',
@@ -15,13 +16,25 @@ export class WorkFlowAddComponent implements OnInit {
 
   private modeler;
 
+  public isVisible = false;
+
+  public validateForm: FormGroup;
+
   private readonly newDiagram = 'assets/bpmn/newDiagram.bpmn';
 
   public saveHref;
 
   public saveName = '';
 
-  constructor(private http: Http, private sanitizer: DomSanitizer) {
+  public formData = {};
+
+  public newDiagramText = null;
+
+  constructor(private fb: FormBuilder, private http: Http, private sanitizer: DomSanitizer) {
+    this.validateForm = this.fb.group({
+      flowname: ['', [Validators.required]],
+      flowType: [null, [Validators.required]]
+    });
   }
 
   ngOnInit() {
@@ -41,7 +54,6 @@ export class WorkFlowAddComponent implements OnInit {
       moddleExtensions: {
         camunda: CamundaModdleDescriptor
       }
-
     });
     this.createDiagram();
   }
@@ -67,6 +79,11 @@ export class WorkFlowAddComponent implements OnInit {
         console.error(err);
       } else {
         this.setEncoded(xml, 'bpmn.xml');
+        console.log(xml);
+        // this.newDiagram = xml;
+        // this.newDiagram = xml;
+        this.newDiagramText = xml;
+        this.isVisible = true;
       }
     });
     e.preventDefault();
@@ -87,11 +104,42 @@ export class WorkFlowAddComponent implements OnInit {
 
   setEncoded(data, name) {
     const encodedData = encodeURIComponent(data);
-
     if (data) {
       this.saveHref = this.sanitizer.bypassSecurityTrustResourceUrl('data:application/bpmn20-xml;charset=UTF-8,' + encodedData);
       this.saveName = name;
     }
   }
 
+  handleOk(): void {
+    console.log('Button ok clicked!');
+
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[ i ].markAsDirty();
+      this.validateForm.controls[ i ].updateValueAndValidity();
+    }
+
+    if (this.validateForm.valid) {
+      this.isVisible = false;
+    }
+    this.validateForm.value['flow'] = this.newDiagramText;
+    console.log(this.validateForm.value);
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+
+  /**
+   * 方法用途: 重置表单
+   * 参数:  事件
+   **/
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.validateForm.reset();
+    for (const key in this.validateForm.controls) {
+      this.validateForm.controls[key].markAsDirty();
+      this.validateForm.controls[key].updateValueAndValidity();
+    }
+  }
 }
