4e62b94fdd429563e984c8d92efe8982b9ad2d84
lidejian <892379244@qq.com>
2019-05-14 22:39:40 +0800

    通用字典

--- src/app/sys/macros/macros.component.ts
+++ src/app/sys/macros/macros.component.ts
@@ -15,6 +15,31 @@ export class MacrosComponent implements OnInit {
     subTitle: '字典列表'
   };
 
+  /**
+   * 属性描述: 分页组建参数
+   * 参数：
+   **/
+  formData: any = {
+    totalNum: null,
+    pageSize: 10,
+    currentPage: 1,
+    name: ''
+  };
+
+  /**
+   * 表头
+   */
+  listHeader = [
+    {title: '用户名', field: 'username', type: 'text', class: 'text-success'},
+    {title: '昵称', field: 'nickname', type: 'text'},
+    {title: '邮箱', field: 'email', type: 'number'},
+    {title: '电话', field: 'mobile', type: 'text'},
+    {title: '上次登录时间', field: 'lastLoginTime', type: 'phone'},
+    {title: '操作', field: 'option', type: 'opt', width: '20%'}
+  ];
+
+  macros: Array<any> = [];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/sys/macros/macros.module.ts
+++ src/app/sys/macros/macros.module.ts
@@ -1,20 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { MacrosRoutingModule } from './macros-routing.module';
-import { MacrosComponent } from './macros.component';
-import { MacroEditComponent } from './macro-edit/macro-edit.component';
+import {MacrosRoutingModule} from './macros-routing.module';
+import {MacrosComponent} from './macros.component';
+import {MacroEditComponent} from './macro-edit/macro-edit.component';
 import {MacroAddComponent} from './macro-add/macro-add.component';
-import {NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
 import {HttpsUtils} from '../../utils/HttpsUtils.service';
 import {CrumbsModule} from '../../../public/crumbs/crumbs.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BailyTableModule} from '../../../public/baily-table/baily-table.module';
+import {PaginationModule} from '../../../public/pagination/pagination.module';
+import {ListModule} from '../../../public/list/list.module';
 
 @NgModule({
   declarations: [MacrosComponent, MacroEditComponent, MacroAddComponent],
   imports: [
     CommonModule,
     MacrosRoutingModule,
-    CrumbsModule
+    CrumbsModule,
+    FormsModule,
+    BailyTableModule,
+    CrumbsModule,
+    PaginationModule,
+    ListModule,
+    ReactiveFormsModule,
+    NgZorroAntdModule,
   ],
   providers: [{provide: NZ_I18N, useValue: zh_CN}, HttpsUtils]
 })
