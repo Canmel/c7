9ad689eb0ca30526eeccfb9ad1e16d18ad290368
lidejian <892379244@qq.com>
2019-11-07 17:42:08 +0800

    头像

--- src/app/sys/profile/profile.component.ts
+++ src/app/sys/profile/profile.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Properties} from '../../../public/properties';
 import {HttpsUtils} from '../../utils/HttpsUtils.service';
 import * as $ from 'jquery';
+import {NzNotificationService} from 'ng-zorro-antd';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-profile',
@@ -13,6 +15,10 @@ export class ProfileComponent implements OnInit {
   crumbs = {
     title: '用户详情'
   };
+
+  imageIndex = [];
+
+  avatarImage = '';
 
   userDetails = {
     username: sessionStorage.getItem(Properties.SESSION.CURRENT_USER_NAME),
@@ -26,31 +32,19 @@ export class ProfileComponent implements OnInit {
     {operation: '删除用户', gmtCreate: '208-10-12 09:21:22', module: '用户 张三 删除用户 "祝覅是" '}
   ];
 
-  constructor(public http: HttpsUtils) {
+  constructor(public http: HttpsUtils, public notification: NzNotificationService) {
   }
 
   ngOnInit() {
+    this.initImageIndex();
     this.loadMe();
-    this.initAvtar();
   }
 
-  initAvtar() {
-    const $box = document.getElementById('pic_box');
-    const $li = $box.getElementsByTagName('li');
-    for (let i = 0; i < $li.length; i++) {
-      console.log($li[i]);
-      $($li[i]).click(function () {
-        $.each($li, function (j, item) {
-          $(item).css('borderRadius', '15%');
-        });
-        this.style.borderRadius = '50%';
-      });
+  initImageIndex() {
+    for ( let i = 1; i <= 35; i++) {
+      this.imageIndex.push(i);
     }
-
-    $('.pic_box li img').click(function () {
-      const src = $(this).attr('src');
-      $('.jide').val(src);
-    });
+    console.log(this.imageIndex);
   }
 
   loadMe() {
@@ -71,19 +65,16 @@ export class ProfileComponent implements OnInit {
   }
 
   querenClickHandler() {
-    const src = $('.jide').val();
-
-    if (src !== '') {
-      $('.my_pic').attr('src', src.toString());
+    if (this.avatarImage !== '') {
       $('.pic_box').animate({
         'top': '-1000px'
       }, 500);
       const user = JSON.parse(sessionStorage.getItem('sysUser'));
-      user['avatar'] = src.toString();
+      user['avatar'] = this.avatarImage;
 
       sessionStorage.setItem('sysUser', JSON.stringify(user));
-      this.http.post('system/sysUser/avatar', {avatar: src}).then(resp => {
-        console.log(resp);
+      this.http.put('system/sysUser', {uid: user['uid'], avatar: this.avatarImage}).then(resp => {
+        this.notification.success('成功', resp['msg']);
       });
     } else {
       return false;
@@ -96,6 +87,17 @@ export class ProfileComponent implements OnInit {
       return user['avatar'];
     }
     return 'assets/img/avatar/1.png';
+  }
+
+  imageClick($event) {
+    console.log($event);
+    const $box = document.getElementById('pic_box');
+    const $li = $box.getElementsByTagName('li');
+    for (let i = 0; i < $li.length; i++) {
+      $($li[i]).css('borderRadius', '15%');
+    }
+    this.avatarImage = $($event.srcElement).attr('src');
+    $($event.srcElement).parents('li').css('borderRadius', '50%');
   }
 
 }
